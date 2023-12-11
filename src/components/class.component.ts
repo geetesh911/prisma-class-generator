e224616e299766b4f0f72cd4c89a5f667911bad6
Geetesh Laddha <geeteshladdha@wimoku.com>
2023-12-11 19:28:20 +0530

    feat: initial commit

---
+++ src/components/class.component.ts
@@ -0,0 +1,35 @@
+import { Echoable } from '../interfaces/echoable'
+import { FieldComponent } from './field.component'
+import { CLASS_TEMPLATE } from '../templates/class.template'
+import { BaseComponent } from './base.component'
+
+export class ClassComponent extends BaseComponent implements Echoable {
+	name: string
+	fields?: FieldComponent[]
+	relationTypes?: string[]
+	enumTypes?: string[] = []
+	extra?: string = ''
+	extends?: string = ''
+	types?: string[]
+
+	echo = () => {
+		const fieldContent = this.fields.map((_field) => _field.echo())
+		let str = CLASS_TEMPLATE.replace(
+			'#!{DECORATORS}',
+			this.echoDecorators(),
+		)
+			.replace('#!{NAME}', `${this.name}`)
+			.replace('#!{FIELDS}', fieldContent.join('\r\n'))
+			.replace('#!{EXTRA}', this.extra)
+			.replace(
+				'#!{EXTENDS}',
+				this.extends ? `extends ${this.extends}` : '',
+			)
+
+		return str
+	}
+
+	reExportPrefixed = (prefix: string) => {
+		return `export class ${this.name} extends ${prefix}${this.name} {}`
+	}
+}
---
+++ src/templates/class.template.ts
@@ -0,0 +1,8 @@
+export const CLASS_TEMPLATE = `#!{IMPORTS}
+
+#!{DECORATORS}
+export class #!{NAME} #!{EXTENDS} {
+#!{FIELDS}
+}
+#!{EXTRA}
+`
